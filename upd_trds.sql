8241f442746f41b3be3760b35ecdc121c557b45e
Niladri Datta <dattaniladri@gmail.com>
2014-04-21 21:14:52 +0530

    Adding trades for 2014-04-21

---
+++ upd_trds.sql
@@ -0,0 +1,21 @@
+\echo Delete old Trade Backup
+DROP TABLE IF EXISTS TRADE_BAK;
+
+\echo
+\echo Create new Trade Backup
+CREATE TABLE TRADE_BAK AS
+	SELECT * FROM TRADE;
+
+\echo
+\echo Updating Trades ...
+\echo
+
+\i ins_trd_21-04.sql;
+
+\echo
+\echo TOTAL TRADES
+SELECT count(*) FROM TRADE;
+
+\echo CALCULATE POSITIONS
+\echo
+\i crt_tab_pos.sql;
